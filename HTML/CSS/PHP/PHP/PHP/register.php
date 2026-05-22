e726017fef4f28482cdb80c4fc51e2de8dfbf675
vrlll4 <rana0567153386@gmail.com>
2026-05-22 13:44:26 +0300

    Update register.php

--- HTML/CSS/PHP/PHP/PHP/register.php
+++ HTML/CSS/PHP/PHP/PHP/register.php
@@ -1,16 +1,25 @@
 <?php
 
+// استدعاء ملف الاتصال بالداتابيس
 include 'connect.php';
 
+// أخذ البيانات القادمة من الفورم
 $name = $_POST['name'];
 $email = $_POST['email'];
+
+/*
+INSERT INTO
+تستخدم لإضافة بيانات داخل الجدول
+*/
 
 $sql = "INSERT INTO customers(name,email)
 
 VALUES('$name','$email')";
 
+// تنفيذ الاستعلام
 mysqli_query($conn,$sql);
 
+// رسالة نجاح
 echo "Data inserted successfully";
 
 ?>
