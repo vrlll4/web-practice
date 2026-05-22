142cea6f2ef9628341bc5ea23a321556dacd4d1e
vrlll4 <rana0567153386@gmail.com>
2026-05-22 13:44:56 +0300

    Update database.sql

--- HTML/CSS/PHP/PHP/PHP/DATABASE/database.sql
+++ HTML/CSS/PHP/PHP/PHP/DATABASE/database.sql
@@ -1,13 +1,19 @@
+-- إنشاء قاعدة البيانات
 CREATE DATABASE dgwtech;
 
+-- استخدام قاعدة البيانات
 USE dgwtech;
 
+-- إنشاء جدول العملاء
 CREATE TABLE customers(
 
+-- رقم العميل ويتزايد تلقائيًا
 id INT AUTO_INCREMENT PRIMARY KEY,
 
+-- اسم العميل
 name VARCHAR(100),
 
+-- إيميل العميل
 email VARCHAR(100)
 
 );
